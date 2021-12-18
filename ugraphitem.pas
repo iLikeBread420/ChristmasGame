9ee9a65797db8858c5a6a0082f0d2453f28f0942
Fabian.R <fabian.ruttmann@gmail.com>
2021-12-18 12:54:07 +0100

    Added graphical class for entities

---
+++ ugraphitem.pas
@@ -0,0 +1,41 @@
+unit ugraphitem;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SDL2, SDL2_Image;
+
+type
+  TEntityDisplay = class
+    strict private
+      mainTexture: PSDL_Texture;
+      renderer: PSDL_Renderer;
+      rect: TSDL_Rect;
+      entity: TEntity;
+    public
+      constructor create(rendr: PSDL_Renderer; img: string, width: int; height: int);
+      procedure draw();
+      destructor destroy(); override;
+  end;
+
+implementation
+
+constructor TEntityDisplay.create(rendr: PSDL_Renderer; img: string, width: int; height: int);
+begin
+  renderer := rendr;
+  mainTexture := IMG_LoadTexture(renderer, img);
+  rect.w := width;
+  rect.h := height;
+end;
+
+procedure TEntityDisplay.draw();
+begin
+  rect.x := entity.getPosX;
+  rect.y := entity.getPosY;
+  SDL_RenderCopy(renderer, mainTexture, nil, @rect);
+end;
+
+end.
+
