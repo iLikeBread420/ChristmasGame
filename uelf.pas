566f43154a332e16cb9abad820f0f768f1c72704
Fabian.R <fabian.ruttmann@gmail.com>
2022-01-07 20:03:04 +0100

    And yet again

--- uelf.pas
+++ uelf.pas
@@ -13,23 +13,16 @@ uses
 
     TElf = class(TEntity)
     strict private
-<<<<<<< HEAD
       kindofnpc : string;       //important cause one elf wont be an enemy but and npc to talk to in room 2
     public
       constructor create(nm: string; maxHealth: integer; spd: integer; konpc : string);
       function getKindofNpc() : string;
-=======
-
-    public
-
->>>>>>> 89c0f54b7f69281b65ee52fddc14caa14df67bda
     end;
 
 implementation
 
 { TElf }
 
-<<<<<<< HEAD
 constructor TElf.create(nm: string; maxHealth: integer; spd: integer;konpc : string);
 begin
   inherited create(nm, maxHealth, spd);
@@ -41,7 +34,5 @@ begin
   Result := kindofnpc;
 end;
 
-=======
->>>>>>> 89c0f54b7f69281b65ee52fddc14caa14df67bda
 end.
 
