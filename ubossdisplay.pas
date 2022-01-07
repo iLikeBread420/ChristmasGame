6a342027d40aea4c959b1783826f1427379efc13
iLikeBread420 <76873040+iLikeBread420@users.noreply.github.com>
2022-01-07 12:36:07 +0100

    new units

---
+++ ubossdisplay.pas
@@ -0,0 +1,18 @@
+unit ubossdisplay;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TBossDisplay = class
+
+  end;
+
+implementation
+
+end.
+
---
+++ uelfdisplay.pas
@@ -0,0 +1,13 @@
+unit uelfdisplay;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+implementation
+
+end.
+
