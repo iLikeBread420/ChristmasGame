fbca8f3daec3b963e6eb29a8d07ca2bc7dd77e19
iLikeBread420 <76873040+iLikeBread420@users.noreply.github.com>
2022-01-07 12:32:12 +0100

    new classes

--- uboss.pas
+++ uboss.pas
@@ -1,4 +1,4 @@
-unit uelf;
+unit uboss;
 
 {$mode objfpc}{$H+}
 
@@ -7,11 +7,8 @@ interface
 uses
   Classes, SysUtils;
 
-  Type
-
-    { TElf }
-
-    TElf = class
+type
+  TBoss = class
     strict private
       name: string;
       maxHP: integer;
@@ -19,10 +16,9 @@ uses
       posX: integer;
       posY: integer;
       speed: integer;
-      kindofnpc : string;
       velocity: array [0..1] of integer;
     public
-      constructor create(nm: string; maxHealth: integer; spd: integer; konpc : string);
+      constructor create(nm: string; maxHealth: integer; spd: integer);
       procedure changeHP(value: integer);
       procedure move(x: integer; y: integer);
       procedure setPos(x: integer; y: integer);
@@ -30,24 +26,21 @@ uses
       function getPosX(): integer;
       function getPosY(): integer;
       function getSpeed(): integer;
-      function getKindofNpc() : string;
-
-    end;
+  end;
 
 implementation
 
-{ TElf }
 
-
-constructor TElf.create(nm: string; maxHealth: integer; spd: integer;konpc : string);
+constructor TBoss.create(nm: string; maxHealth: integer; spd: integer);
 begin
   name := nm;
   maxHP := maxHealth;
   hp := maxHealth;
   speed := spd;
-  konpc := kindofnpc;
 end;
-procedure TElf.changeHP(value: integer);
+
+// Diese Funktion fuegt Lebenspunkte hinzu oder zieht welche ab.
+procedure TBoss.changeHP(value: integer);
 begin
   hp := hp + value;
   if hp < 0 then
@@ -58,42 +51,37 @@ begin
 end;
 
 // Entity in eine bestimmte Richtung bewegen (relativ zur momentanen Position)
-procedure TElf.move(x: integer; y: integer);
+procedure TBoss.move(x: integer; y: integer);
 begin
   posX := posX + x;
   posY := posY + y;
 end;
 
 // Absolute Position der Entity setzen
-procedure TElf.setPos(x: integer; y: integer);
+procedure TBoss.setPos(x: integer; y: integer);
 begin
   posX := x;
   posY := y;
 end;
 
-function TElf.getHP(): integer;
+function TBoss.getHP(): integer;
 begin
   Result := hp;
 end;
 
-function TElf.getPosX(): integer;
+function TBoss.getPosX(): integer;
 begin
   Result := posX;
 end;
 
-function TElf.getPosY(): integer;
+function TBoss.getPosY(): integer;
 begin
   Result := posY;
 end;
-function TElf.getSpeed(): integer;
+
+function TBoss.getSpeed(): integer;
 begin
   Result := speed;
 end;
-
-function TElf.getKindofNpc(): string;
-begin
-  Result := kindofnpc;
-end;
-
 end.
 
